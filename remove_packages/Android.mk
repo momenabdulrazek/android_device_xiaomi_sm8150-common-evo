94c97680813f3b2f34901520b3421e2e5efea2ce
kuroringo901 <ak4700@gmx.com>
2023-08-28 14:22:04 +0200

    sm8150-common: gapps debloat

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Updater
+LOCAL_OVERRIDES_PACKAGES := Updater PrebuiltGoogleTelemetryTvp WallpaperEmojiPrebuilt ScribePrebuilt WellbeingPrebuilt GooglePrintRecommendationService RecorderPrebuilt PartnerSetupPrebuilt HelpRtcPrebuilt SoundAmplifierPrebuilt WallpaperEffect SecurityHubPrebuilt SafetyHubPrebuilt EmergencyInfoGoogleNoUi talkback TurboPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
